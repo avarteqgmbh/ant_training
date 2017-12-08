11a8e450ec9b1a8a37de2c87998e330ec2764eb4
Maxime de la Fouchardière <ratouney1998@gmail.com>
2017-12-08 17:28:37 +0100

    Setup (#1)
    Add ESLint
    Add AntDesign
    Add Redux-Logger
    Update README

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+const sampleReducer = (state, action) => {
+  return state;
+}
+
+export default sampleReducer;
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { 
+  createStore,
+  applyMiddleware,
+} from 'redux';
+import { createLogger } from 'redux-logger'; 
+import appReducer from './reducers/';
+
+const logger = createLogger({
+  timestamp: false,
+  collapsed: true,
+});
+
+const store = createStore(
+  appReducer,
+  applyMiddleware(logger)
+);
+
+export default store;
